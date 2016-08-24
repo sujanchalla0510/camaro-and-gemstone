b54e1af0d50ed6f55e57ab045a41a1d8292c39a0
sujan kumar reddy challa <sujankumar0510@gmail.com>
2016-08-24 01:40:23 -0700

    Update gemstone.rb

--- gemstone.rb
+++ gemstone.rb
@@ -1,11 +1,7 @@
 lines = $stdin.gets.to_i
-
 char_counts = Hash.new
-
 lines.times do
-
   visted_chars =  Hash.new
-
   line = $stdin.gets.chomp
   line.unpack('C*').each do |char|
     if visted_chars[char].nil?
@@ -14,10 +10,8 @@ lines.times do
       visted_chars[char] = true
     end
   end
-
 end
 
 output = []
 char_counts.each { |key, value| output << key if value == lines   }
-
 puts output.length
